47ec0cca0226f0e419fef0868bb62815ebafbdbb
Adv Usman Haidar Sial <108092688+Advusmansial@users.noreply.github.com>
2023-03-18 19:31:53 +0500

    Create LocalStorage.sol

---
+++ LocalStorage.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+// Creating a contract
+contract Test {
+
+// Defining function to calculate sum of 2 numbers
+function add() public view returns(uint){
+    uint num1 = 10;
+    uint num2 = 16;
+    uint sum =n um1 + num2;
+    return sum;
+}
+}
